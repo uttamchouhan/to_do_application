077e07d7590b9d234dce5cd5c733dd5b77b22170
Uttam Chouhan <cuttam9892@gmail.com>
2025-04-16 18:16:16 +0530

    Added in repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-[95%] max-w-7xl mx-auto my-5 px-2'>
+      <h1 className='text-2xl font-bold text-green-400'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
